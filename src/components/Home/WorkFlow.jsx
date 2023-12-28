174f77302498cdf79dedde3a89d340ec8b4a1315
Shivanshu <pshivanshu99@gmail.com>
2023-12-28 18:43:21 +0530

    workflow

--- src/components/Home/WorkFlow.jsx
+++ src/components/Home/WorkFlow.jsx
@@ -10,54 +10,62 @@ const WorkFlow = () => {
     <div className='text-white bg-white w-[100%]  py-5 flex flex-col items-center justify-center  gap-[3%]'>
       <div className=' w-[100%] sm:w-[70%] h-[5%] text-black sm:text-4xl text-2xl text-center flex items-center justify-center mb-5'>Enhance Your Workflow with Integrated Tools</div>
       {/* The Circle Code Starts from here  */}
-      <div className='relative w-[100%] sm:w-[60%]  flex items-center justify-center'>
+      <div className='relative w-[100%] min-h-[450px]  sm:w-[60%]  flex items-center justify-center'>
 
       <div className='absolute  sm:w-[32%] w-[40%] left-[-5px] sm:left-0  top-0 h-full flex flex-col justify-between'>
-        <div className='absolute top-0  left-0 w-[100%] h-[45%] flex flex-col items-center justify-between z-20'>
-          <div className='h-[40%] w-full flex justify-center'>
+        <div className='    w-[100%] h-[45%] flex flex-col items-center justify-center z-20'>
+          <div className='h-[30%] sm:h-[40%] w-full flex justify-center '>
             <img src={wf1} width={'27%'} alt="Left Pillar Top Image" />
           </div>
-          <div className='w-[100%]  text-black flex flex-col items-center justify-center'>
+          <div className='w-[100%] h-[70%] sm:h-[60%] text-black flex flex-col items-center justify-start'>
             <div className='sm:text-lg text-[15px] text-center font-semibold'>Messaging Platform</div>
             <div className='sm:text-[15px] text-[13px] leading-4 sm:leading-5 text-gray-800 text-center'>Stay connected and collaborate seamlessly with clients and team members.</div>
           </div>
         </div>
-        <div className='absolute bottom-0  left-0 w-[100%] h-[45%] flex flex-col items-center justify-between z-20'>
+        <div className='    w-[100%] h-[45%] flex flex-col items-center justify-center z-20'>
+          <div className='h-[30%] sm:h-[40%] w-full flex justify-center '>
+            <img src={wf3} width={'27%'} alt="Left Pillar Top Image" />
+          </div>
+          <div className='w-[100%] h-[70%] sm:h-[60%] text-black flex flex-col items-center justify-start'>
+            <div className='sm:text-lg text-[15px] text-center font-semibold'>CRM System</div>
+            <div className='sm:text-[15px] text-[13px] leading-4 sm:leading-5 text-gray-800 text-center'>Manage client relationships effectively with our user-friendly CRM system.</div>
+          </div>
+        </div>
+
+        {/* <div className=' bg-pink-800  left-0 w-[100%] h-[45%] flex flex-col items-center justify-center z-20'>
           <div className='h-[40%] w-full flex justify-center'>
             <img src={wf3} width={'27%'} alt="Left Pillar Top Image" />
           </div>
           <div className='w-[100%]  text-black flex flex-col items-center justify-center'>
             <div className='sm:text-lg text-[15px] text-center font-semibold'>CRM System</div>
-            <div className='sm:text-[15px] text-[13px] leading-4 sm:leading-5 text-gray-800 text-center'>Manage client relationships effectively with our user-friendly CRM system.</div>
+            <div className='sm:text-[15px] text-[13px] leading-4 sm:leading-5 text-gray-800 text-center'></div>
           </div>
-        </div>
+        </div> */}
       </div>
-
-      <div className='absolute   sm:w-[32%] w-[40%] right-0  top-0 h-full flex flex-col justify-between sm:gap-5 gap-10 '>
-        <div className='    w-[100%] h-[45%] flex flex-col items-center justify-between z-20'>
-          <div className='h-[40%] w-full flex justify-center'>
+      <div className='absolute  sm:w-[32%] w-[40%] right-0   top-0 h-full flex flex-col justify-between sm:gap-5 gap-10'>
+        <div className='    w-[100%] h-[45%] flex flex-col items-center justify-center z-20'>
+          <div className='h-[30%] sm:h-[40%] w-full flex justify-center '>
             <img src={wf2} width={'27%'} alt="Left Pillar Top Image" />
           </div>
-          <div className='w-[100%]  text-black flex flex-col items-center justify-center'>
-            <div className='sm:text-lg text-[15px] text-center  leading-4 font-semibold'>Collaborative Workspace</div>
+          <div className='w-[100%] h-[70%] sm:h-[60%] text-black flex flex-col items-center justify-start'>
+            <div className='sm:text-lg text-[15px] text-center font-semibold'>Collaborative Workspace</div>
             <div className='sm:text-[15px] text-[13px] leading-4 sm:leading-5 text-gray-800 text-center'>Experience the power of collaboration in our shared workspace .</div>
           </div>
         </div>
-        <div className='absolute bottom-0  mt-10 left-0 w-[100%] h-[45%]  flex flex-col items-center  justify-between z-20'>
-          <div className='h-[40%] w-full flex justify-center'>
+        <div className='    w-[100%] h-[45%] flex flex-col items-center justify-center z-20'>
+          <div className='h-[30%] sm:h-[40%] w-full flex justify-center '>
             <img src={wf4} width={'27%'} alt="Left Pillar Top Image" />
           </div>
-          <div className='w-[100%]  text-black flex flex-col items-center justify-center'>
+          <div className='w-[100%] h-[70%] sm:h-[60%] text-black flex flex-col items-center justify-start'>
             <div className='sm:text-lg text-[15px] text-center font-semibold'>Calendar Integration</div>
             <div className='sm:text-[15px] text-[13px] leading-4 sm:leading-5 text-gray-800 text-center'>Simplify your scheduling and time management with our calendar integration.</div>
           </div>
-        </div> 
+        </div>
       </div>
 
-     
       {/* Circle */}
-      <div className='w-[55vh] h-[55vh]    rounded-full  flex items-center justify-center'>
-        <div className='sm:w-[35vh] sm:h-[35vh] w-[22vh] h-[22vh] rounded-full'>
+      <div className='rounded-full  flex items-center justify-center'>
+        <div className='sm:w-[35vh] sm:h-[35vh] w-[200px] h-[200px] rounded-full'>
           <img src={img} alt="Circular Image" />
         </div>
       </div>
